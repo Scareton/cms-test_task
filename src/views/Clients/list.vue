1a5e90de36ed9ce13062d7668d753e88994b5f9b
Scareton <voron4288@gmail.com>
2021-12-13 10:37:23 +0300

    table manager slot

--- src/views/Clients/list.vue
+++ src/views/Clients/list.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="clients">
-    <b-table  hover :items="clientsList" class="clients-table"></b-table>
+    <b-table hover :items="clientsList" :fields="fields" class="clients-table">
+      <template #cell(manager)="data">{{ data.item.manager.name }}</template>
+    </b-table>
   </div>
 </template>
 
 <script>
 export default {
   name: "ClientsList",
+  data() {
+    return {
+      fields: [
+        { key: "id", label: "#" },
+        { key: "name", label: "ФИО" },
+        { key: "manager", label: "Менеджер" },
+      ],
+    };
+  },
   computed: {
     clientsList() {
-      return this.$store.getters['clients/listWithManagers']
+      return this.$store.getters["clients/listWithManagers"];
     },
   },
 };
